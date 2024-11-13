7fdaf4e12b6c7620553485906a80762d941630fa
BharathChavala1 <bharath.chavala123@gmail.com>
2024-11-13 16:39:33 -0600

    Intial commit

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'bharathchavala417@gmail.com',
+    phone: '+1(417)-351-7352',
+    address: 'Springfield, Missouri, United States -65804',
+
+    sheetAPI: 'https://script.google.com/macros/s/AKfycbxCcWU5bkcT2tdotw2CwOm1CaOrmVqR0FRKSI25Z-iuM6bIgpZaUswEwRsNc1TtDSqUbg/exec'
+}
---
+++ src/data/experienceData.js
@@ -0,0 +1,16 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Tata Consultancy Services',
+        jobtitle: 'Software Engineer',
+        startYear: '2021',
+        endYear: '2022'
+    },
+    {
+        id: 2,
+        company: 'Aadyam Solutions',
+        jobtitle: 'Associate Software Engineer',            
+        startYear: '2020',
+        endYear: '2021'
+    }
+]
