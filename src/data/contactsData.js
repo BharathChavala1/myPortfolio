cdd2c9692dda650704cb15d504c30491421eaa57
BharathChavala1 <bharath.chavala123@gmail.com>
2024-11-13 20:03:27 -0600

    updates

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -3,5 +3,5 @@ export const contactsData = {
     phone: '+1(417)-351-7352',
     address: 'Springfield, Missouri, United States -65804',
 
-    sheetAPI: 'https://script.google.com/macros/s/AKfycbxCcWU5bkcT2tdotw2CwOm1CaOrmVqR0FRKSI25Z-iuM6bIgpZaUswEwRsNc1TtDSqUbg/exec'
+    sheetAPI: 'https://script.google.com/macros/s/AKfycbzk2Xud3FSR7RX3tJYM-gTAB0e83SoEzsG1sPvFdDw_ef5w_YQOFLBE8ywGuHuGakxWJw/exec'
 }
