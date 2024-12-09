3eecbeeddd27316a7c94d31dbdbe80d981aad0df
BharathChavala1 <bharath.chavala123@gmail.com>
2024-12-09 10:23:02 -0600

    Removed cached gh-pages directory

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,16 +1,23 @@
 export const experienceData = [
-    {
-        id: 1,
-        company: 'Tata Consultancy Services',
-        jobtitle: 'Software Engineer',
-        startYear: '2021',
-        endYear: '2022'
-    },
-    {
-        id: 2,
-        company: 'Aadyam Solutions',
-        jobtitle: 'Associate Software Engineer',            
-        startYear: '2020',
-        endYear: '2021'
-    }
-]
+  {
+    id: 1,
+    company: "American Express Technology",
+    jobtitle: "Software Engineer",
+    startYear: "2023",
+    endYear: "Present",
+  },
+  {
+    id: 2,
+    company: "Dell Technologies",
+    jobtitle: "Software Engineer",
+    startYear: "2020",
+    endYear: "2022",
+  },
+  {
+    id: 3,
+    company: "Sonata Technologies",
+    jobtitle: "Software Engineer",
+    startYear: "2019",
+    endYear: "2020",
+  },
+];
