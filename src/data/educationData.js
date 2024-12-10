7c45f381b43cdd93549a2fae3827f7d262dbaec8
BharathChavala1 <bharath.chavala123@gmail.com>
2024-12-10 08:04:58 -0600

    Initial commit

---
+++ src/data/educationData.js
@@ -0,0 +1,16 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'Missouri State University',
+        course: 'Master of Technology',
+        startYear: '2022',
+        endYear: '2024'
+    },
+    {
+        id: 2,
+        institution: 'Gandhi Institute Of Technology',
+        course: 'Bachelor of Technology',
+        startYear: '2016',
+        endYear: '2020'
+    },
+]
---
+++ src/data/projectsData.js
@@ -0,0 +1,82 @@
+import one from '../assets/svg/projects/one.svg'
+import two from '../assets/svg/projects/two.svg'
+import three from '../assets/svg/projects/three.svg'
+import four from '../assets/svg/projects/four.svg'
+import five from '../assets/svg/projects/five.svg'
+import six from '../assets/svg/projects/six.svg'
+import seven from '../assets/svg/projects/seven.svg'
+import eight from '../assets/svg/projects/eight.svg'
+
+
+export const projectsData = [
+    {
+        id: 1,
+        projectName: 'Messaging Application',
+        projectDesc: 'Experience seamless real-time messaging with our Spring-powered chat app.',
+        tags: ['RabbitMQ','Redis','WebSocket'],
+        code: 'https://github.com/BharathChavala1/Messaging_App.git',
+        demo: 'https://github.com/BharathChavala1/Messaging_App.git',
+        image: one
+    },
+    {
+        id: 2,
+        projectName: 'Food Delivery API',
+        projectDesc: 'Power your food delivery service with our robust API, enabling seamless ordering, tracking, and delivery management for restaurants and delivery partners',
+        tags: ['Java','Spring Boot','MySQL'],
+        code: 'https://github.com/BharathChavala1/Food_Devlivery_Project.git',
+        demo: 'https://github.com/BharathChavala1/Food_Devlivery_Project.git',
+        image: two
+    },
+    {
+        id: 3,
+        projectName: 'E-Commerce Microservice Architecture',
+        projectDesc: 'This project outlines a microservices architecture for an e-commerce platform. The system utilizes Docker containers orchestrated by Kubernetes for deployment and management.',
+        tags: ['Microservice Architecture','Kafka','Docker','Grafana'],
+        code: 'https://github.com/BharathChavala1/E-Commerce-Microservice-Architecture.git',
+        demo: 'https://github.com/BharathChavala1/E-Commerce-Microservice-Architecture.git',
+        image: three
+    },
+    {
+        id: 4,
+        projectName: 'E-Commerce App',
+        projectDesc: 'A simple User Friendly E-commerce application',
+        tags: ['Spring Data JPA', 'Hibernate','MySQL'],
+        code: 'https://github.com/BharathChavala1/E-Commerce.git',
+        demo: 'https://github.com/BharathChavala1/E-Commerce.git',
+        image: four
+    },
+    {
+        id: 5,
+        projectName: 'Spring Boot Employee Data Management System',
+        projectDesc: 'A Spring Boot application for managing employee data with RESTful APIs, using lambda expressions for efficient data processing and external service integration.',
+        tags: ['Java', 'Spring Boot', 'MySQL', 'Spring Data JPA'],
+        code: 'https://github.com/BharathChavala1/Spring-Employee-Project.git',
+        demo: 'https://github.com/BharathChavala1/Spring-Employee-Project.git',
+        image: five
+    },
+    {
+        id: 6,
+        projectName: 'Order Management with Redis Caching and Pub/Sub',
+        projectDesc: 'A Spring-based Order Management system using Redis for efficient caching and real-time Pub/Sub messaging, enhancing performance and enabling instant order updates across services.',
+        tags: ['Spring Boot','Java','Redis','Spring MVC'],
+        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        image: six
+    },
+]
+
+
+// Do not remove any fields.
+// Leave it blank instead as shown below
+
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
