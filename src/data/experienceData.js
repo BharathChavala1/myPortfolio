15f7eb946f456ff9d06167f933b2990a7db2e1b7
BharathChavala1 <bharath.chavala123@gmail.com>
2024-12-10 22:33:38 -0600

    Initial commit

---
+++ src/data/experienceData.js
@@ -0,0 +1,23 @@
+export const experienceData = [
+  {
+    id: 1,
+    company: "American Express Technology",
+    jobtitle: "Software Engineer",
+    startYear: "2023",
+    endYear: "Present",
+  },
+  {
+    id: 2,
+    company: "Dell Technologies",
+    jobtitle: "Software Engineer",
+    startYear: "2020",
+    endYear: "2022",
+  },
+  {
+    id: 3,
+    company: "Sonata Technologies",
+    jobtitle: "Software Engineer",
+    startYear: "2019",
+    endYear: "2020",
+  },
+];
---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import Bharath_Chavala_Software_Engineer from "../assets/pdf/Bharath_Chavala_Software_Engineer.pdf";
+import image from "../assets/images/BharathChavala.JPG";
+export const headerData = {
+  name: "Bharath Chavala",
+  title: "Software Engineer",
+  desciption:
+    "I believe in Programming isn't about what you know, it's about what you can figure out. - Chris Pine",
+  image: image,
+  resumePdf: Bharath_Chavala_Software_Engineer,
+};
