9c78f197c21c6a78b74b3850dfc1bd07e5633976
BharathChavala1 <bharath.chavala123@gmail.com>
2024-12-10 21:58:52 -0600

    Updates

--- src/data/contactsData.js
+++
@@ -1,8 +0,0 @@
-export const contactsData = {
-  email: "bharathchavala417@gmail.com",
-  phone: "+1(417)-351-7352",
-  address: "Springfield, Missouri, United States -65804",
-
-  sheetAPI:
-    "https://script.google.com/macros/s/AKfycbzMnpKlNPkAXVmeCXLl729b7TQsJSwp1poxhlVKotmvzaZCX7F2MU0YvkbyepXVsn9dCA/exec",
-};
