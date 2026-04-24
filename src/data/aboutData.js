e58c29f5a1e6aa11571f1bf1e8f3b4f8cd81c67d
BharathChavala1 <bharath.chavala123@gmail.com>
2026-04-23 21:54:24 -0400

    Latest changes

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,8 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "My name's Bharath Chavala. I'm a Software Engineer based in Springfield, MO US.",
-    description2: "I'm a passionate software engineer with a knack for crafting robust and scalable solutions. My journey in the world of programming began 5 years ago, and since then, I've been on a quest to merge creativity with functionality. I thrive in dynamic environments where innovation and collaboration are key. With a strong foundation in Java, Spring Boot, and microservices, I enjoy tackling complex challenges and turning them into elegant, user-centric solutions. When I'm not coding, you can find me exploring the latest tech trends, honing my skills, or contributing to open-source projects. I believe that continuous learning is the cornerstone of growth, and I'm always eager to expand my knowledge and expertise. Let's connect and explore how we can innovate together!",
-    image: 2
-}
+  title: "Who I am",
+  description1:
+    "My name is Bharath Chavala, and I'm a Full Stack Java Developer based in Princeton, NJ, USA.",
+  description2:
+    "I'm a passionate Full Stack Java Developer with a knack for crafting robust and scalable solutions. My journey in the world of programming began 7+ years ago, and since then, I've been on a quest to merge creativity with functionality. I thrive in dynamic environments where innovation and collaboration are key. With a strong foundation in Java, Spring Boot, and microservices, I enjoy tackling complex challenges and turning them into elegant, user-centric solutions. When I'm not coding, you can find me exploring the latest tech trends, honing my skills, or contributing to open-source projects. I believe that continuous learning is the cornerstone of growth, and I'm always eager to expand my knowledge and expertise. Let's connect and explore how we can innovate together!",
+  image: 2,
+};
